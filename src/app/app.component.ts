e8119e10486c6f94702e4fc09a953e5545fcb32d
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-12-16 13:42:19 -0300

    Initializing some components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BackgroundPageComponent } from './components/background-page/background-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BackgroundPageComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-portfolio-remake';
 }
---
+++ src/app/components/background-page/background-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './background-page.component.html',
+  styleUrl: './background-page.component.scss'
+})
+export class BackgroundPageComponent {
+
+}
